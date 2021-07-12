5df69d4e98993187d870131fa036b21b4c32749d
buktop72 <jasonx2007@yandex.ru>
2021-07-12 19:20:15 +0400

    stocks_products add filters

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-
     'logistic',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -140,4 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
