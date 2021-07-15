60b6e02f414802788a326979be20653389aabeb8
buktop72 <jasonx2007@yandex.ru>
2021-07-15 17:52:31 +0400

    init api_with_restrictions

--- stocks_products/logistic/models.py
+++ stocks_products/logistic/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=60, unique=True)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return f'{self.id} - {self.title} - {self.description}'
